7dc335865a5156bf8209f01ab1841bf196959f40
Adrián Arnaiz <aar0062@alu.ubu.es>
2019-04-04 13:54:06 +0200

    Instalación para Windows
    Se instalan los requerimientos mediante un script python, así funciona también para windows.

---
+++ install.py
@@ -0,0 +1,53 @@
+''' Requerimientos mínimos:
+playsound==1.2.2
+dash_html_components==0.7.0
+pysptk==0.1.7
+plotly==2.0.15
+sounddevice==0.3.11
+scipy==1.0.0
+pandas==0.22.0
+easygui==0.98.1
+dash_core_components==0.12.5
+matplotlib==2.2.2
+dash==0.18.3
+numpy==1.14.0
+scikit_learn==0.19.1'''
+import os
+print('\n \n \n Instalar pysptk:')
+os.system('pip install pysptk')
+
+print('\n \n \n Instalar playsound:')
+os.system('pip install playsound')
+
+print('\n \n \n Instalar dash_html_components:')
+os.system('pip install dash_html_components')
+
+print('\n \n \n Instalar plotly:')
+os.system('pip install plotly')
+
+print('\n \n \n Instalar sounddevice:')
+os.system('pip install sounddevice')
+
+print('\n \n \n Instalar scipy:')
+os.system('pip install scipy')
+
+print('\n \n \n Instalar pandas:')
+os.system('pip install pandas')
+
+print('\n \n \n Instalar easygui:')
+os.system('pip install easygui')
+
+print('\n \n \n Instalar dash_core_components:')
+os.system('pip install dash_core_components')
+
+print('\n \n \n Instalar matplotlib:')
+os.system('pip install matplotlib')
+
+print('\n \n \n Instalar dash:')
+os.system('pip install dash')
+
+print('\n \n \n Instalar numpy:')
+os.system('pip install numpy')
+
+print('\n \n \n Instalar scikit_learn:')
+os.system('pip install scikit_learn')
